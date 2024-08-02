5d9c4dcd66fbac06a1ad91285cafaa99be6a5224
ahronsinger <singerahron@gmail.com>
2024-08-02 06:15:00 -0400

    change get_quote to class method

--- backend/services.py
+++ backend/services.py
@@ -2,35 +2,28 @@ import requests
 import os
 
 class StockQuoteService:
-    def __init__(self):
-        self.api_key = os.getenv('API_KEY')
-        self.base_url = 'https://www.alphavantage.co/query'
+    api_key = os.getenv('API_KEY')
+    base_url = 'https://www.alphavantage.co/query'
 
-    
-    def get_quote(self, symbol):
+    @classmethod
+    def get_quote(cls, symbol):
         params = {
             'function': 'GLOBAL_QUOTE',
             'symbol': symbol,
-            'apikey': self.api_key
+            'apikey': cls.api_key
         }
         
         try:
-            response = requests.get(self.base_url, params=params)
+            response = requests.get(cls.base_url, params=params)
             data = response.json()
 
             if 'Global Quote' in data and data['Global Quote']:
                 price = data['Global Quote']['05. price']
                 formatted_price = f"{float(price):,.2f}"
             else:
-                raise ValueError(f"Invalid data received for {symbol}")
+                raise ValueError(f"Invalid data received for {symbol}: {data}")
             
         except requests.RequestException as e:
             raise Exception(f"Error fetching stock data: {e}")
         
         return formatted_price
-        
-
-if __name__ == '__main__':
-    stock = StockQuoteService()
-    stock.get_quote('IBM')
-
